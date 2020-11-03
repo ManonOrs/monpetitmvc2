f68065faaf81364861a1e2d1231ccfa23fe52f61
33695 <33695@DESKTOP-9T3U4JI>
2020-11-03 21:42:36 +0100

    navbar + find ids

--- parametres.php
+++ parametres.php
@@ -3,5 +3,5 @@
 define('DATABASE_URL', "localhost");
 define('DATABASE_USER', "orsini");
 define('DATABASE_PWD', "P@ssw0rd");
-define('DATABASE_NAME', "clicom");
+define('DATABASE_NAME', "clicommvc");
 define('CNSTRING', "mysql:host=" . DATABASE_URL . ";dbname=" .DATABASE_NAME . ";charset=UTF8" );
--- src/Controller/GestionClientController.php
+++ src/Controller/GestionClientController.php
@@ -2,20 +2,33 @@
 
 namespace APP\Controller;
 
+use APP\Model\GestionClientModel;
+use ReflectionClass;
+use Exception;
+use Tools\MyTwig;
+use Tools\Repository;
+use APP\Entity\Client;
+
 class GestionClientController {
     
-    public function chercheUn($params){
-        // appel de la méthode find($id) de la classe Model adequate
-        $modele = new GestionClientModel();
-        $id = filter_var(intval($params["id"]), FILTER_VALIDATE_INT);
-        $unClient = $modele->find($id);
-        if ($unClient){
-            $r = new ReflectionClass($this);
-            include_once PATH_VIEW . str_replace ('Controller', 'View', $r->getShortName()). "/unClient.php";
-        }else {
-            throw new Exception("Client " . $id . " inconnu");
-        }
+     public function chercheUn(array $params): void {
+    $repository = Repository::getRepository("APP\Entity\Client");
+    $ids = $repository->findIds();
+    // on place ces ids dans le tableau de paramètres que l'on va envoyer à la vue
+    $params['lesId'] = $ids;
+    // on teste si l'id du client à chercher a été passé dans l'URL
+    if (array_key_exists('id', $params)) {
+      $id = filter_var(intval($params['id']), FILTER_VALIDATE_INT);
+      $unClient = $repository->find($id);
+      // on place le client trouvé dans le tableau de paramètres que l'on va envoyer à la vue
+      $params['unClient'] = $unClient;
     }
+    $r = new ReflectionClass($this);
+    $vue = str_replace('Controller', 'View', $r->getShortName()) . "/unClient.html.twig";
+    MyTwig::afficheVue($vue, $params);
+  }
+  
+  
     public function chercheTous(){
         //appel de la méthode findAll() de la classe Model adequate
         $modele = new GestionClientModel();
@@ -26,6 +39,26 @@ class GestionClientController {
         } else {
             throw new Exception("Auccun Client à afficher");
         }
+    }
+    
+    public function creerClient(array $params) {
+    if(empty($params)){
+    $vue = "GestionClientView\\creerClient.html.twig";
+    MyTwig::afficheVue($vue, array());
+    } else {
+      // creation de l'objet client
+      $client = new Client($params);
+      $repository = Repository::getRepository("APP\Entity\Client");
+      $repository->insert($client);
+      $this->chercheTous();
+    }
+    }
+    
+     public function enregistreClient(array $params) {
+    // creation de l'objet Client
+    $client = new Client($params);
+    $modele = new GestionClientModel();
+    $modele->enregistreClient($client);
     }
     
 }
